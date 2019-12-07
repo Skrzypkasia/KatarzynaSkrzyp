f6d9449faca2be82abc4150c3f3f8aee8ff3a460
Kasia <superkasia3@gmail.com>
2019-12-07 12:31:08 +0100

    mnozenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int a=3, b=4, suma;
+	int a=3, b=4, suma, mnozenie;
 	suma=a+b;
         System.out.println("Suma wynosi:"+suma);
+        mnozenie=a+b;
+        System.out.println("mnożenie wynosi" +mnozenie);
+        
 
 
     }
