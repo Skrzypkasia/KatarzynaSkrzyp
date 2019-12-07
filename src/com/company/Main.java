837465fc9810bbbf66734bca58a63277eaee00cf
Kasia <superkasia3@gmail.com>
2019-12-07 12:29:07 +0100

    dzielenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int a=3, b=4, suma;
+	int a=3, b=4, suma, dzielenie;
 	suma=a+b;
         System.out.println("Suma wynosi:"+suma);
+        dzielenie=a/b;
+        System.out.println("iloczyn wynosi:"+suma);
+
 
 
     }
